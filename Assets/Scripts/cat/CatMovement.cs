6731f1697aca585d9cb29f613bfc33e2ae7734a6
Manu <manuelgalvez192@gmail.com>
2025-02-08 00:02:45 +0100

    flip del gato

--- Assets/Scripts/cat/CatMovement.cs
+++ Assets/Scripts/cat/CatMovement.cs
@@ -3,19 +3,26 @@ using UnityEngine;
 public class CatMovement : MonoBehaviour
 {
     public float speed = 5f;
+    public float rotationSpeed = 10f;
     private Vector3 moveDirection;
+    private Quaternion targetRotation;
 
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        
-        moveDirection = new Vector3(horizontal - vertical, 0, vertical + horizontal).normalized;
-        transform.position += moveDirection * speed * Time.deltaTime;
 
-        if (moveDirection != Vector3.zero)
+        moveDirection = new Vector3(horizontal - vertical, 0, vertical + horizontal).normalized;
+
+        if (horizontal != 0)
         {
-            
+            float angle = horizontal < 0 ? 132f : -48f;
+            targetRotation = Quaternion.Euler(0, angle, 0);
         }
+        if (Quaternion.Angle(transform.rotation, targetRotation) > 180f)
+            targetRotation = Quaternion.Euler(0, targetRotation.eulerAngles.y - 360f, 0);
+        
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        transform.position += moveDirection * speed * Time.deltaTime;
     }
 }
