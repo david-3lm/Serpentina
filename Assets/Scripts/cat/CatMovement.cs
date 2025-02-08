33a6a21018e37a72f9c06b8a6256c5092e796aa2
Manu <manuelgalvez192@gmail.com>
2025-02-08 15:30:56 +0100

    movimiento algo fixeado

--- Assets/Scripts/cat/CatMovement.cs
+++ Assets/Scripts/cat/CatMovement.cs
@@ -25,28 +25,33 @@ public class PlayerController : MonoBehaviour
         
         animator.SetBool("isWalking", moveDirection != Vector3.zero);
         
-        if (horizontal > 0 && !facingRight)
+        if (horizontal > 0)
         {
             facingRight = true;
             animator.SetBool("backWalk", false);
             targetRotation = Quaternion.Euler(0, -40, 0);
         }
-        else if (horizontal < 0 && facingRight)
+        else if (horizontal < 0)
         {
             facingRight = false;
             animator.SetBool("backWalk", false);
             targetRotation = Quaternion.Euler(0, 140, 0);
         }
-        else if (vertical > 0 && !facingRight)
+        else if (vertical > 0)
         {
             animator.SetBool("backWalk", true);
             targetRotation = Quaternion.Euler(0, 140, 0);
         }
-        else if (vertical < 0 && facingRight)
+        else if (vertical < 0)
         {
             facingRight = true;
             animator.SetBool("backWalk", false);
-            targetRotation = Quaternion.Euler(0, 140, 0);
+            targetRotation = Quaternion.Euler(0, -40, 0);
+        }
+        else if (vertical > 0 && horizontal < 0)
+        {
+            animator.SetBool("backWalk", true);
+            
         }
         
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
