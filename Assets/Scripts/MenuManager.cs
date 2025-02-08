37301d272502dbcbf4cdd69ed109682414d765c1
DevLopz <d.3lopm@gmail.com>
2025-02-08 04:55:28 +0100

    Menu armario y baldosa

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Mantiene el objeto en todas las escenas
+        }
+        else
+        {
+            Destroy(gameObject); // Elimina duplicados si ya existe una instancia
+        }
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
